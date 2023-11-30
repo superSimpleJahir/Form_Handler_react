f76f8fc7f5f63d358cd5ff21404567507e22ff77
Jahir <supersimple.jahir@gmail.com>
2023-11-30 20:33:55 +0600

    [Done] delete some file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#live link:
+# live link:
--- src/components/formData/FormData.jsx
+++ src/components/formData/FormData.jsx
@@ -1,4 +1,3 @@
-//! 2 Task: Create a FormData components, the form contains input fields like  Cloth Name, Cloth Id, Price, Quantity, Description, a select menu that contains three colors as options like "red","blue","green" , add terms and condition with checkbox, add date field Manufacture date and add three radio button of Size "M", "L" and "XL".
 
 import { useEffect, useState } from "react";
 import ProductList from "../ProductList/ProductList";
